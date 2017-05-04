6ae786b9aaebb94b4174d6f34fdd12000d026d61
quietsamurai98 <quietsamurai98@gmail.com>
2017-05-04 17:41:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ function instant_runoff_vote(candidateArrayINPUT, ballotArrayINPUT){
     roundNumber++;
     irv_printRound(candidateArray, runningTally, roundNumber);
     while(Math.max( ... runningTally) < votesToWin){
-        var lowestCandidate = candidateArray[runningTally.indexOf(Math.min( ... runningTally))];
+        var runningTallyIgnoreEliminated = runningTally.map(function(val, i) {
+            return val === 0 ? votesToWin*4 : val;
+        });
+        
+        var lowestCandidate = candidateArray[runningTally.indexOf(Math.min( ... runningTallyIgnoreEliminated))];
         irv_eliminate(lowestCandidate, ballotArray);
         runningTally = irv_tally(candidateArray, ballotArray);
         roundNumber++;
@@ -49,3 +53,24 @@ function irv_printRound(candidateArray, runningTally, roundNumber){
 }
 var testBallots = [["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["0","3","2","1"],["1","0","2","3"],["1","0","2","3"],["1","0","2","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["1","2","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["2","1","0","3"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","1","2"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","0","2","1"],["3","1","0","2"],["3","1","0","2"],["3","1","0","2"],["3","1","0","2"],["3","1","0","2"],["3","1","0","2"]]; //Taken from http://condorcet.ericgorr.net/
 var testCandidates = ["0","1","2","3"];
+
+function randomBallots(candidateArray, numberOfBallots){ //Generate an array of ranked ballots with candidates in candidateArray, and length numberOfBallots
+    var ballotArr = [];
+    for(var n=0; n<numberOfBallots; n++){
+        var ballot = candidateArray.slice(0); //Sets ballot to a clone of the candidate array, in order.
+        
+        for (var i = ballot.length - 1; i > 0; i--) { //Shuffles order of candidates using Durstenfeld shuffle algorithm
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = ballot[i];
+            ballot[i] = ballot[j];
+            ballot[j] = temp;
+        }
+        
+        ballotArr[n] = ballot; //Add ballot to list of ballots
+    }
+    return ballotArr;
+}
+
+function ballotArrayToWebFormat(ballotArray){//converts array of ballots to format used by http://www1.cse.wustl.edu/~legrand/rbvote/calc.html
+    
+}
